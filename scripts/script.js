0b8b98b20be49573e564f5ed668e9cd20f542d6b
Elignobis <rwayisibongile@gmail.com>
2022-06-25 13:53:13 +0200

    last commit

--- scripts/script.js
+++ scripts/script.js
@@ -84,26 +84,8 @@ clearBtn.addEventListener('click', ()=> {
 })
 
 
-// Sort items
 
-
-// const sortLS = document.getElementById('sortLS');
-// todoEl.addEventListener("click", () => {
-//     todos.sort((a, b) => {
-//     let fa = a.item.toLowerCase(),
-//         fb = b.item.toLowerCase();
-//       if (fa < fb) {
-//         return -1;
-//       }
-//       if (fa > fb) {
-//         return 1;
-//       }
-//       return 0;
-//     });
-//     showData();
-// });
-
-
+// Sort list
 
 function sortList() {
     var list, i, switching, b, shouldSwitch;
@@ -130,99 +112,8 @@ function sortList() {
 
 
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("");
-  //declaration
-  var txt = document.createTextNode("x");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
 
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("");
-  //declaration
-  var txt = document.createTextNode("x");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("theUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-  //Delete btn
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("x");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-};
-function secondElement() {
-  var list, i, switching, b, shouldSwitch;
-  list = document.getElementById("theUL");
-  switching = true;
-  while (switching) {
-    switching = false;
-    b = list.getElementsByTagName("LI");
-    for (i = 0; i < (b.length - 1); i++) {
-      shouldSwitch = false;
-      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
-      switching = true;
-      console.log(b[i].innerHTML);
-    }
-  }
-}
+
+
 
